80bffc74712e1807bd6cd5f1f24cc649be4270ed
Mark Vivian <meliodasdragonsin008@gmail.com>
2023-02-24 23:39:44 +0300

    added a function which will show the content components when they are clicked understood the event.target

--- website-react/src/Components/Content/Content.js
+++ website-react/src/Components/Content/Content.js
@@ -2,21 +2,43 @@ import Chat from './Chat/Chat'
 import CurrentBet from './CurrentBets/CurrentBet'
 import History from './History/History'
 import "./Content.css"
+import { useState } from 'react'
 
 function Content() {
+  const [state, setState] = useState({
+    ChatState : false,
+    CurrentBet : false,
+    History : false
+  })
+  
+  
+  function ChangeState(event){
+    const {name} = event.target
+    console.log(name)
+    setState((item)=>{
+      return{
+        ...item,
+        [name] : true
+      }
+    })
+  }
+  
   return (
     <div className='ContentBox'>
-    
+    Content
       <div>
-          <Chat />
+        <button name='ChatState' onClick={ChangeState}>Chats</button>
+          {state.ChatState && <Chat />}
       </div>
 
       <div>
-         <CurrentBet />
+        <button name='CurrentBet' onClick={ChangeState}>Current Bets</button>
+        { state.CurrentBet && <CurrentBet />}
       </div>
       
       <div>
-         <History />
+        <button name='History' onClick={ChangeState}>History</button>
+         { state.History && <History />}
       </div>
     
     </div>
