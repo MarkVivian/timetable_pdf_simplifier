27fd6f717347556ccd9711a17423c44b63c7c943
Mark Vivian <markviviantech@gmail.com>
2023-04-28 01:35:56 +0300

    done with the footer

--- client/src/MainLayout/Footer.tsx
+++ client/src/MainLayout/Footer.tsx
@@ -1,11 +1,13 @@
 import React from 'react'
 
-function Footer() {
+const Footer:React.FC = () => {
   return (
     <>
-      
+      <footer className=' fixed md:absolute bottom-0 p-4 w-[100%] border-t-2 dark:border-white border-black'>
+        Laborum do anim est cillum Lorem laboris mollit elit sit id consequat. Eu excepteur consectetur est dolor esse ut qui. Consectetur officia nostrud reprehenderit esse officia.
+      </footer>
     </>
   )
 }
 
-export default Footer
+export default Footer;
